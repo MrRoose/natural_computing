6cc238f405cbfa4559759e5f810673afc584b1ff
LESchernthaner <34715592+LESchernthaner@users.noreply.github.com>
2021-04-09 17:50:45 +0200

    Finish A5Q2

--- Assignment5/AS5Q2.py
+++ Assignment5/AS5Q2.py
@@ -1,4 +1,5 @@
 from math import factorial, ceil
+import matplotlib.pyplot as plt
 
 def majVoteProb(c, p):
 	m = ceil((c+1)/2)
@@ -15,11 +16,29 @@ def majVoteProb(c, p):
 	return totalMajProb
 
 def part2b():
-	c = 31
+	c = 30
 	p = 0.6
 	tmp = majVoteProb(c, p)
 	print(f'Probability of correct majority vote for {c} people with individual correctness probability {p}: {round(tmp, 3)}')
 
+def part2c():
+	groupSize = [1,2,3,4,5,6,7,8,9,10,15,20,25,30,35,40,45,50]
+	pRadiologist = 0.85
+	pDoctor = 0.75
+	pMedStudent = 0.6
+	tmpR = [majVoteProb(gs, pRadiologist) for gs in groupSize]
+	tmpD = [majVoteProb(gs, pDoctor) for gs in groupSize]
+	tmpM = [majVoteProb(gs, pMedStudent) for gs in groupSize]
+
+	plt.plot(groupSize, tmpR, color='green', label='Radiologist')
+	plt.plot(groupSize, tmpD, color='red', label='Doctor')
+	plt.plot(groupSize, tmpM, color='blue', label='Medical student')
+	plt.legend()
+	plt.xlabel('Group size')
+	plt.ylabel('Probability correct majority vote')
+	plt.savefig('A5Q2C.png')
+	plt.show()
 
 if __name__ == "__main__":
 	part2b()
+	part2c()
