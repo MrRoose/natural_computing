3c4f947e36808d2ee7164ffdc5536350e49daebe
Daan <daaneo@live.nl>
2021-06-10 11:52:09 +0200

    Move files to right directory.

--- project/TTPExperiment.py
+++ project/TTPExperiment.py
@@ -1,11 +1,10 @@
 import os
-import re
 import timeit
 import logging
 import sys
 from typing import List
 
-from jmetal.core.solution import BinarySolution, PermutationSolution, CompositeSolution
+from TTPevaluate import get_experiment_solutions
 from jmetal.lab.experiment import Job
 
 
@@ -21,7 +20,7 @@ class TTPExperiment:
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='[%(asctime)s] %(levelname)s - %(message)s')
 
     def run(self) -> None:
-
+        total_start_time = timeit.default_timer()
         for job in self.jobs:
             logging.debug(f'Executing job {job.run_tag}')
             start_time = timeit.default_timer()
@@ -29,50 +28,15 @@ class TTPExperiment:
             job.execute(output_path=output_path)
             elapsed = timeit.default_timer() - start_time
             logging.debug(f'Finished job {job.run_tag} in {elapsed} seconds.')
+        elapsed = timeit.default_timer() - total_start_time
+        logging.debug(f'Finished all jobs in {elapsed} seconds.')
 
     def get_solutions(self):
         job_solutions = []
         for job in self.jobs:
             output_path = os.path.join(self.output_dir, job.algorithm_tag, job.problem_tag)
             experiment_name = job.run_tag
-            job_solutions.append(self.__get_experiment_solutions(output_path, experiment_name))
+            job_solutions.append(get_experiment_solutions(output_path, experiment_name))
         return job_solutions
 
-    def __get_experiment_solutions(self, dir, experiment_name):
-        solutions = []
-        with open(f'{dir}\\VAR.{experiment_name}.tsv') as f:
-            for line in f:
-                tour = []
-                bitstring = []
 
-                variables = re.findall(r"\[(.*?)\]", line)
-                str_tour = variables[0]
-                str_bitstring = variables[3]
-                for word in str_tour.split(','):
-                    word = word.strip()
-                    tour.append(int(word))
-                for word in str_bitstring.split(','):
-                    if 'True' in word:
-                        bitstring.append(True)
-                    elif 'False' in word:
-                        bitstring.append(False)
-                    else:
-                        print('Error, no true or false found.')
-                binary_solution = BinarySolution(number_of_variables=1, number_of_objectives=2)
-
-                permutation_solution = PermutationSolution(number_of_variables=len(tour), number_of_objectives=2)
-                binary_solution.variables = bitstring
-                permutation_solution.variables = tour
-                solution = CompositeSolution([permutation_solution, binary_solution])
-                solutions.append(solution)
-
-        with open(f'{dir}\\FUN.{experiment_name}.tsv') as f:
-            line_idx = 0
-            for line in f:
-                objective_vals = line.strip().split(' ')
-                if len(objective_vals) != 2:
-                    break
-                solutions[line_idx].objectives[0] = float(objective_vals[0])
-                solutions[line_idx].objectives[1] = float(objective_vals[1])
-                line_idx += 1
-        return solutions
--- project/experiments.py
+++ project/experiments.py
@@ -1,186 +1,82 @@
 from jmetal.algorithm.multiobjective import SPEA2
-from jmetal.core.quality_indicator import HyperVolume
-from jmetal.lab.visualization import Plot
 from jmetal.operator import NullCrossover
-from jmetal.core.solution import BinarySolution, PermutationSolution, CompositeSolution
 from jmetal.operator.crossover import PMXCrossover, CompositeCrossover, SPXCrossover
 from jmetal.operator.mutation import PermutationSwapMutation, BitFlipMutation, CompositeMutation
-from jmetal.util.solution import get_non_dominated_solutions
 
-from jmetal.util.termination_criterion import StoppingByEvaluations, StoppingByTime
-from jmetal.lab.experiment import Experiment, Job, generate_summary_from_experiment
+from jmetal.util.termination_criterion import StoppingByEvaluations
+from jmetal.lab.experiment import Job
 
-import re
-import timeit
 
 from TTPExperiment import TTPExperiment
 from TTPProblem import TTPProblem
+from TTPevaluate import read_and_write_hypervolumes, create_plots
 
 
-def configure_experiment(problem):
+def configure_experiment(problem, n_runs=31):
     jobs = []
-    max_seconds = 10800
+    max_evaluations = 10000
 
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), SPXCrossover(0.8)]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='SPX-0,8_run1',
+    for run in range(n_runs):
+        jobs.append(
+            Job(
+                algorithm=SPEA2(
+                    problem=problem,
+                    population_size=500,
+                    offspring_population_size=500,
+                    mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
+                    crossover=CompositeCrossover([PMXCrossover(0.8), NullCrossover()]),
+                    termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
+                ),
+                algorithm_tag='SPEA',
+                problem_tag='TTP',
+                run=f'NullCrossover_run{str(run)}',
+            )
         )
-    )
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), SPXCrossover(0.8)]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='SPX-0,8_run2',
+
+        jobs.append(
+            Job(
+                algorithm=SPEA2(
+                    problem=problem,
+                    population_size=500,
+                    offspring_population_size=500,
+                    mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
+                    crossover=CompositeCrossover([PMXCrossover(0.8), SPXCrossover(0.2)]),
+                    termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
+                ),
+                algorithm_tag='SPEA',
+                problem_tag='TTP',
+                run=f'SPX-0,2_run{str(run)}',
+            )
         )
-    )
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), SPXCrossover(0.8)]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='SPX-0,8_run3',
+        jobs.append(
+            Job(
+                algorithm=SPEA2(
+                    problem=problem,
+                    population_size=500,
+                    offspring_population_size=500,
+                    mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
+                    crossover=CompositeCrossover([PMXCrossover(0.8), SPXCrossover(0.8)]),
+                    termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
+                ),
+                algorithm_tag='SPEA',
+                problem_tag='TTP',
+                run=f'SPX-0,8_run{str(run)}',
+            )
         )
-    )
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), SPXCrossover(0.2)]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='SPX-0,2_run1',
-        )
-    )
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), SPXCrossover(0.2)]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='SPX-0,2_run2',
-        )
-    )
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), SPXCrossover(0.2)]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='SPX-0,2_run3',
-        )
-    )
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), NullCrossover()]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='NullCrossover_run1',
-        )
-    )
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), NullCrossover()]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='NullCrossover_run2',
-        )
-    )
-    jobs.append(
-        Job(
-            algorithm=SPEA2(
-                problem=problem,
-                population_size=500,
-                offspring_population_size=500,
-                mutation=CompositeMutation([PermutationSwapMutation(1.0 / problem.n_cities), BitFlipMutation(0.1)]),
-                crossover=CompositeCrossover([PMXCrossover(0.8), NullCrossover()]),
-                termination_criterion=StoppingByTime(max_seconds=max_seconds)
-            ),
-            algorithm_tag='SPEA',
-            problem_tag='TTP',
-            run='NullCrossover_run3',
-        )
-    )
 
     return jobs
+
 
 fn = 'eil51_n50_bounded-strongly-corr_01.ttp'
 problem = TTPProblem(fn, dropping_rate=1, p_picking=0.05)
 jobs = configure_experiment(problem=problem)
-
-# Run the study
 output_directory = 'experiments'
 experiment = TTPExperiment(output_dir=output_directory, jobs=jobs)
-start_time = timeit.default_timer()
 experiment.run()
-elapsed = timeit.default_timer() - start_time
-print(f'{elapsed} seconds for the 9 jobs')
 solutions = experiment.get_solutions()
 
+read_and_write_hypervolumes('experiments/SPEA/TTP')
+create_plots('experiments/SPEA/TTP')
 
 
-
-# solutions = get_TTPsolutions('experiments/SPEA/TTP/', 'SPX-0,8')
-# front = get_non_dominated_solutions(solutions)
-# hypervolume_qi = HyperVolume([1.0,1.0])
-# print(hypervolume_qi.compute([solutions[i].objectives for i in range(len(solutions))]))
-# for solution in front:
-#     solution.objectives[1] *= -1
-# plot_front = Plot(title='Pareto front approximation', axis_labels=['tour length', 'value'])
-# plot_front.plot(front, label='Multiobjective TTP', filename='TTP', format='png')
 
